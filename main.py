5b91fce06563ce7dd2ecbdec9a769d36d24556f8
MS2104 <31002@ma-web.nl>
2022-10-26 12:27:12 +0200

    the j

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ newgreeting.greet()
 
 
 def basicHaiku():
-    return ["Toward those short trees.","We saw a hawk descending","On a day in spring."]
+    return ["Toward those short trees.","Dit is een verandering","On a day in spring."]
 
 #zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
 
